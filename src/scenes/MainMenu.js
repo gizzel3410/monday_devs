ca622ab259c63d9e0d3bee6abd5d3fb925f20045
arstah <arvid.stromberg@lm.se>
2024-09-26 09:01:35 +0200

    gå automatiskt till cardgameScene

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -9,16 +9,17 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.text(512, 460, 'Spela', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('CardGameScene');
-
-        });
+        this.scene.start('CardGameScene');
+        //
+        // this.add.text(512, 460, 'Spela', {
+        //     fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+        //     stroke: '#000000', strokeThickness: 8,
+        //     align: 'center'
+        // }).setOrigin(0.5);
+        //
+        // this.input.once('pointerdown', () => {
+        //
+        //
+        // });
     }
 }
