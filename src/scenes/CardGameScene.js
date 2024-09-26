3106d38c6474cb33707a83478cf2669fdc85e007
arstah <arvid.stromberg@lm.se>
2024-09-26 13:48:58 +0200

    enklaste gameloopen jag kan tänkta mig

--- src/scenes/CardGameScene.js
+++ src/scenes/CardGameScene.js
@@ -3,20 +3,28 @@ export class CardGameScene extends Phaser.Scene {
         super({key: 'CardGameScene'});
     }
 
+    cardSize = {w: 150, h: 175}
+
     create() {
 
         this.player = {
+            interactable: true,
+            life: 3,
             hand: [],
             pool: [],
             board: [],
+            discard: [],
             handArea: {x: 0, y: this.scale.height - 150, height: 150},
             cardArea: {x: 0, y: this.scale.height - 350, height: 150}
         }
 
         this.enemy = {
+            interactable: false,
+            life: 3,
             hand: [],
             pool: [],
             board: [],
+            discard: [],
             handArea: {x: 0, y: 0, height: 150},
             cardArea: {x: 0, y: 200, height: 150}
         }
@@ -24,9 +32,17 @@ export class CardGameScene extends Phaser.Scene {
         this.drawBoard()
 
         // Fill pool with some cards
-        for (let i = 0; i < 10; i++) {
-            this.player.pool.push({id: i, color: Phaser.Display.Color.RandomRGB()});
-            this.enemy.pool.push({id: i, color: Phaser.Display.Color.RandomRGB()});
+        for (let i = 0; i < 40; i++) {
+            this.player.pool.push({
+                id: i,
+                color: Phaser.Display.Color.RandomRGB(),
+                damage: Math.floor(Math.random() * 10) + 1
+            });
+            this.enemy.pool.push({
+                id: i,
+                color: Phaser.Display.Color.RandomRGB(),
+                damage: Math.floor(Math.random() * 10) + 1
+            });
         }
 
         // Draw initial hand of 5 cards
@@ -34,8 +50,9 @@ export class CardGameScene extends Phaser.Scene {
 
         // Add Drag Events
         this.input.on('dragstart', (pointer, gameObject) => {
-            gameObject.setFillStyle(0x00ff00); // Change color instead of using setTint
-            this.player.handArea.nbCards -= 1;
+            gameObject.list[0].setFillStyle(0x00ff00); // Change color instead of using setTint
+            gameObject.oldX = gameObject.x;
+            gameObject.oldY = gameObject.y;
         });
 
         this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
@@ -44,7 +61,7 @@ export class CardGameScene extends Phaser.Scene {
         });
 
         this.input.on('dragend', (pointer, gameObject) => {
-            gameObject.setFillStyle(0xffffff); // Reset to original color
+            gameObject.list[0].setFillStyle(0xffffff); // Reset to original color
 
             console.log(`${pointer.x}, ${pointer.y}`);
 
@@ -52,7 +69,6 @@ export class CardGameScene extends Phaser.Scene {
 
                 const index = this.player.hand.findIndex(card => card.card.id === gameObject.card.id);
                 const cardObject = this.player.hand.splice(index, 1)[0];
-                console.log(cardObject.card.id);
 
                 cardObject.x = 100 + this.player.board.length * 160;
                 cardObject.y = this.player.cardArea.y + 50;
@@ -62,9 +78,8 @@ export class CardGameScene extends Phaser.Scene {
                 this.reDrawHand(this.player)
                 this.nextTurn()
             } else {
-                gameObject.x = 100 + this.player.handArea.nbCards * 160;
-                gameObject.y = this.player.handArea.y + 100;
-                this.player.handArea.nbCards += 1;
+                gameObject.x = gameObject.oldX;
+                gameObject.y = gameObject.oldY;
             }
 
 
@@ -79,22 +94,52 @@ export class CardGameScene extends Phaser.Scene {
         }
     }
 
-    reDrawHand(owner){
-        console.log(owner.hand.length)
+    reDrawHand(owner) {
         for (let i = 0; i < owner.hand.length; i++) {
             const card = owner.hand[i];
             card.x = 100 + i * 160;
         }
     }
 
+    discardHand(owner){
+        console.log('discardHand')
+        while (owner.board.length > 0) {
+            const container = owner.board[0];        // Get the first item
+            container.x = 0;
+            container.y = 0;
+
+            // Make the container visually invisible but still functional
+            container.setAlpha(0); // Makes the container and its children invisible
+            container.disableInteractive(); // Disables interaction for the container
+
+            owner.discard.push(container);
+            owner.board.shift();               // Remove the first item
+        }
+    }
+
     addCardToHand(owner) {
         const card = owner.pool.pop();
-        const cardObject = this.add.rectangle(100 + owner.hand.length * 160, owner.handArea.y + 100, 150, 175, card.color.color);
-        cardObject.setInteractive();  // Make the card interactive
-        this.input.setDraggable(cardObject);  // Enable dragging for this card
-        cardObject.card = card;
+        const container = this.add.container(100 + owner.hand.length * 160, owner.handArea.y + 100);
+        const cardObject = this.add.rectangle(0, 0, this.cardSize.w, this.cardSize.h, card.color.color);
 
-        owner.hand.push(cardObject);
+        const numberText = this.add.text(0, 0, `${card.damage}`, {
+            fontSize: '32px',
+            fill: '#090808',
+            align: 'center'
+        });
+
+        container.add(cardObject);
+        container.add(numberText);
+        container.setSize(this.cardSize.w, this.cardSize.h);
+
+        if (owner.interactable) {
+            container.setInteractive();
+            this.input.setDraggable(container);
+        }
+
+        container.card = card;
+
+        owner.hand.push(container);
         owner.handArea.nbCards += 1;
     }
 
@@ -116,9 +161,45 @@ export class CardGameScene extends Phaser.Scene {
         enemyCardRect.fillRect(this.enemy.cardArea.x, this.enemy.cardArea.y, this.scale.width, this.enemy.cardArea.height);
     }
 
+    totalTurnNumber = 0;
+    turnNumber = 0;
+
     nextTurn() {
-        console.log('next turn!')
+        this.totalTurnNumber++;
+        this.turnNumber++;
+
+        console.log('turn: ' + this.totalTurnNumber);
+
         this.enemyMove()
+
+        if (this.turnNumber >= 5) {
+            console.log('calc')
+            const playerDamage = this.player.board.reduce((sum, card) => sum + card.card.damage, 0);
+            const enemyDamage = this.enemy.board.reduce((sum, card) => sum + card.card.damage, 0);
+
+            console.log(`player: ${playerDamage} vs enemy: ${enemyDamage}`);
+            if (playerDamage > enemyDamage) {
+                this.enemy.life--;
+                console.log(`enemy takes damage: ${this.enemy.life} / 3`)
+            }
+            if (enemyDamage > playerDamage) {
+                this.player.life--;
+                console.log(`player takes damage: ${this.player.life} / 3`)
+            }
+
+            if (this.player.life <= 0) {
+                console.log('player dies...')
+            }
+            if (this.enemy.life <= 0) {
+                console.log('enemy dies...')
+            }
+
+            this.discardHand(this.player)
+            this.discardHand(this.enemy)
+            this.drawCards(5)
+
+            this.turnNumber = 0;
+        }
     }
 
     enemyMove() {
