682b8c517ff1c0015d5a272f886d78932d33aa23
arstah <arvid.stromberg@lm.se>
2024-09-26 12:02:10 +0200

    refaktoriserat till player och enemy objekt

--- src/scenes/CardGameScene.js
+++ src/scenes/CardGameScene.js
@@ -3,39 +3,30 @@ export class CardGameScene extends Phaser.Scene {
         super({key: 'CardGameScene'});
     }
 
-    // playerHandArea = {x: 0, y: 0};
-    // playerCardArea = {x: 0, y: 0};
-    //
-    // enemyHandArea = {x: 0, y: 0};
-    // enemyCardArea = {x: 0, y: 0};
-
     create() {
-        this.playerHandArea = {x: 0, y: this.scale.height - 150, height: 150, nbCards: 0};
-        this.playerCardArea = {x: 0, y: this.scale.height - 350, height: 150, nbCards: 0};
-
-        this.enemyHandArea = {x: 0, y: 0, height: 150, nbCards: 0};
-        this.enemyCardArea = {x: 0, y: 200, height: 150, nbCards: 0};
-
-        this.playerHand = [];
-        this.playerPool = [];
-
-        this.enemyHand = [];
-        this.enemyPool = [];
 
         this.player = {
             hand: [],
             pool: [],
-            handArea: {x: 0, y: this.scale.height - 150, height: 150, nbCards: 0},
-            cardArea: {x: 0, y: this.scale.height - 350, height: 150, nbCards: 0}
+            board: [],
+            handArea: {x: 0, y: this.scale.height - 150, height: 150},
+            cardArea: {x: 0, y: this.scale.height - 350, height: 150}
         }
 
-        this.board = [];
+        this.enemy = {
+            hand: [],
+            pool: [],
+            board: [],
+            handArea: {x: 0, y: 0, height: 150},
+            cardArea: {x: 0, y: 200, height: 150}
+        }
+
         this.drawBoard()
 
         // Fill pool with some cards
         for (let i = 0; i < 10; i++) {
-            this.playerPool.push({id: i, color: Phaser.Display.Color.RandomRGB()});
-            this.enemyPool.push({id: i, color: Phaser.Display.Color.RandomRGB()});
+            this.player.pool.push({id: i, color: Phaser.Display.Color.RandomRGB()});
+            this.enemy.pool.push({id: i, color: Phaser.Display.Color.RandomRGB()});
         }
 
         // Draw initial hand of 5 cards
@@ -44,7 +35,7 @@ export class CardGameScene extends Phaser.Scene {
         // Add Drag Events
         this.input.on('dragstart', (pointer, gameObject) => {
             gameObject.setFillStyle(0x00ff00); // Change color instead of using setTint
-            this.playerHandArea.nbCards -= 1;
+            this.player.handArea.nbCards -= 1;
         });
 
         this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
@@ -57,16 +48,23 @@ export class CardGameScene extends Phaser.Scene {
 
             console.log(`${pointer.x}, ${pointer.y}`);
 
-            if (pointer.y > this.playerCardArea.y && pointer.y < this.playerCardArea.y + this.playerCardArea.height) {
-                console.log('playerCardArea!');
-                gameObject.x = 100 + this.playerCardArea.nbCards * 160;
-                gameObject.y = this.playerCardArea.y + 25;
-                this.playerCardArea.nbCards += 1;
+            if (pointer.y > this.player.cardArea.y && pointer.y < this.player.cardArea.y + this.player.cardArea.height) {
+
+                const index = this.player.hand.findIndex(card => card.card.id === gameObject.card.id);
+                const cardObject = this.player.hand.splice(index, 1)[0];
+                console.log(cardObject.card.id);
+
+                cardObject.x = 100 + this.player.board.length * 160;
+                cardObject.y = this.player.cardArea.y + 50;
+
+                this.player.board.push(cardObject);
+
+                this.reDrawHand(this.player)
                 this.nextTurn()
             } else {
-                gameObject.x = 100 + this.playerHandArea.nbCards * 160;
-                gameObject.y = this.playerHandArea.y + 100;
-                this.playerHandArea.nbCards += 1;
+                gameObject.x = 100 + this.player.handArea.nbCards * 160;
+                gameObject.y = this.player.handArea.y + 100;
+                this.player.handArea.nbCards += 1;
             }
 
 
@@ -75,56 +73,47 @@ export class CardGameScene extends Phaser.Scene {
 
     drawCards(count) {
         for (let i = 0; i < count; i++) {
-            if (this.playerHand.length < 5 && this.playerPool.length > 0) {
-                this.addCardToHandOfPlayer();
-                this.addCardToHandOfEnemy();
-            }
+            console.log('draw cards')
+            this.addCardToHand(this.player);
+            this.addCardToHand(this.enemy);
         }
     }
 
     reDrawHand(owner){
-
+        console.log(owner.hand.length)
+        for (let i = 0; i < owner.hand.length; i++) {
+            const card = owner.hand[i];
+            card.x = 100 + i * 160;
+        }
     }
 
-    addCardToHandOfPlayer() {
-        // const card = owner.hand.pop();
-        const card = this.playerPool.pop();
-        const cardSprite = this.add.rectangle(100 + this.playerHand.length * 160, this.playerHandArea.y + 100, 150, 200, card.color.color);
-        cardSprite.setInteractive();  // Make the card interactive
-        this.input.setDraggable(cardSprite);  // Enable dragging for this card
+    addCardToHand(owner) {
+        const card = owner.pool.pop();
+        const cardObject = this.add.rectangle(100 + owner.hand.length * 160, owner.handArea.y + 100, 150, 175, card.color.color);
+        cardObject.setInteractive();  // Make the card interactive
+        this.input.setDraggable(cardObject);  // Enable dragging for this card
+        cardObject.card = card;
 
-        this.playerHand.push(card);
-        this.playerHandArea.nbCards += 1;
-    }
-
-    addCardToHandOfEnemy() {
-        const card = this.enemyPool.pop();
-        const cardSprite = this.add.rectangle(100 + this.enemyHand.length * 160, this.enemyHandArea.y + 50, 150, 200, card.color.color);
-        cardSprite.setInteractive();  // Make the card interactive
-        this.input.setDraggable(cardSprite);  // Enable dragging for this card
-
-        card.gameObject = cardSprite;
-
-        this.enemyHand.push(card);
-        this.enemyHand.nbCards += 1;
+        owner.hand.push(cardObject);
+        owner.handArea.nbCards += 1;
     }
 
     drawBoard() {
         let playerHandRect = this.add.graphics();
         playerHandRect.fillStyle(0x0000ff, 1); // 0x0000ff is the color code for blue, 1 is full opacity
-        playerHandRect.fillRect(this.playerHandArea.x, this.playerHandArea.y, this.scale.width, this.playerHandArea.height);
+        playerHandRect.fillRect(this.player.handArea.x, this.player.handArea.y, this.scale.width, this.player.handArea.height);
 
         let playerCardRect = this.add.graphics();
         playerCardRect.fillStyle(0x3cb371, 1); // 0x0000ff is the color code for blue, 1 is full opacity
-        playerCardRect.fillRect(this.playerCardArea.x, this.playerCardArea.y, this.scale.width, this.playerCardArea.height);
+        playerCardRect.fillRect(this.player.cardArea.x, this.player.cardArea.y, this.scale.width, this.player.cardArea.height);
 
         let enemyHandRect = this.add.graphics();
         enemyHandRect.fillStyle(0xff0000, 1);
-        enemyHandRect.fillRect(this.enemyHandArea.x, this.enemyHandArea.y, this.scale.width, this.enemyHandArea.height);
+        enemyHandRect.fillRect(this.enemy.handArea.x, this.enemy.handArea.y, this.scale.width, this.enemy.handArea.height);
 
         let enemyCardRect = this.add.graphics();
         enemyCardRect.fillStyle(0xffa500, 1);
-        enemyCardRect.fillRect(this.enemyCardArea.x, this.enemyCardArea.y, this.scale.width, this.enemyCardArea.height);
+        enemyCardRect.fillRect(this.enemy.cardArea.x, this.enemy.cardArea.y, this.scale.width, this.enemy.cardArea.height);
     }
 
     nextTurn() {
@@ -134,20 +123,9 @@ export class CardGameScene extends Phaser.Scene {
 
     enemyMove() {
         console.log('enemy move!')
-        const card = this.enemyHand.pop()
-
-        console.log(`${card.gameObject.x}, ${card.gameObject.y}`);
-
-
-        card.gameObject.x = 100 + this.enemyCardArea.nbCards * 160;
-        card.gameObject.y = this.enemyCardArea.y + 25;
-        this.enemyCardArea.nbCards += 1;
-
-        console.log(`${card.gameObject.x}, ${card.gameObject.y}`);
-
-        // gameObject.x = 100 + this.playerCardArea.nbCards * 160;
-        // gameObject.y = this.playerCardArea.y + 25;
-        // this.playerCardArea.nbCards += 1;
-
+        const cardObject = this.enemy.hand.pop()
+        cardObject.x = 100 + this.enemy.board.length * 160;
+        cardObject.y = this.enemy.cardArea.y + 75;
+        this.enemy.board.push(cardObject)
     }
 }
